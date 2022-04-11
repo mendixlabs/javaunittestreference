4a50737978c8337a3c37480b097f80a7dacbe264
Rik Bos <rik.bos@mendix.com>
2022-04-11 11:53:15 +0200

    Add message on failing of the ApplicationRootUrlTest

--- src/test/java/myfirstmodule/ApplicationRootUrlTest.java
+++ src/test/java/myfirstmodule/ApplicationRootUrlTest.java
@@ -6,13 +6,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- *
- * @author reinout
+ * @author Rik Bos
  */
 public class ApplicationRootUrlTest extends MendixUnitTestBase {
 
     @Test
     public void testApplicationRootURL() {
-        assertEquals("http://localhost:8080", Core.getConfiguration().getApplicationRootUrl());
+        assertEquals("http://localhost:8080",
+                Core.getConfiguration().getApplicationRootUrl(),
+                "Please update the value of the Application Root Url in resources/m2ee.yaml");
     }
 }
