315fac3f8fd54c145b34432c29abf5a0201f9b4d
Reinout van Schouwen <reinout.van.schouwen@mendix.com>
2022-06-02 14:49:41 +0200

    Remove unnecessary CoreProxy
    - Initializing Core with mocked ICore is sufficient
    - move some mocked Core calls to separate methods in MendixUnitTestBase

--- src/test/java/myfirstmodule/actions/MyFirstJavaActionTest.java
+++ src/test/java/myfirstmodule/actions/MyFirstJavaActionTest.java
@@ -1,9 +1,8 @@
 package myfirstmodule.actions;
 
+import com.mendix.core.Core;
 import com.mendix.logging.ILogNode;
 import myfirstmodule.MendixUnitTestBase;
-import myfirstmodule.implementation.CoreProxyImpl;
-import myfirstmodule.interfaces.CoreProxy;
 import static myfirstmodule.proxies.constants.Constants.getMyFirstConstant;
 import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.times;
@@ -15,12 +14,10 @@ import static org.mockito.Mockito.verify;
  */
 public class MyFirstJavaActionTest extends MendixUnitTestBase {
 
-    private final CoreProxy core = CoreProxyImpl.getInstance();
-
     @Test
     public void testMessageIsLogged() {
-        ILogNode log = core.getLogger(getMyFirstConstant()); // gets a handle on the mocked log node
+        ILogNode log = Core.getLogger(getMyFirstConstant()); // gets a handle on the mocked log node
         MyFirstJavaAction.logDemo();
-        verify(log, times(1)).info("CoreProxy usage demonstration");
+        verify(log, times(1)).info("MendixUnitTestBase usage demonstration");
     }
 }
